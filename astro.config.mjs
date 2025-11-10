2d8109099d9171153c1f4a2e06c9cce914eb02f5
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-11-10 13:08:48 -0600

    Removes quotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
         provider: fontProviders.google(),
         name: "Inter",
         cssVariable: "--font-inter",
-        weights: ["400", "600", "700", "800"],
+        weights: [400, 600, 700, 800],
         styles: ["normal"],
         subsets: ["latin"],
       },
